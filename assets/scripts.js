fb10fc7f0f47b7d570476cd4992b07fa40cb66a1
suhwaz <suhail.wazeer@outlook.com>
2024-10-04 22:40:47 +0100

    Updated version of Umrah webiste code

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,28 +1,14 @@
-// Function to toggle package tile details
-function toggleDetails(tile) {
-    tile.classList.toggle('expanded');
-    const description = tile.querySelector('p');
-    if (description.style.display === "block") {
-        description.style.display = "none";
-    } else {
-        description.style.display = "block";
-    }
-}
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const serviceID = 'service_0z1klrr';
+    const templateID = 'template_k13gcwd';
 
-// EmailJS integration
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent default form submission
-
-    // Send email
-    emailjs.sendForm('service_0z1klrr', 'template_k13gcwd', this)
-        .then(function () {
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
             alert('Message sent successfully!');
-        }, function (error) {
-            alert('Failed to send message: ' + JSON.stringify(error));
+            this.reset();
+        }, (err) => {
+            alert('Failed to send message: ' + JSON.stringify(err));
         });
 });
-
-// Initialize EmailJS
-(function () {
-    emailjs.init('Ib3347M_GbhwVWh7p'); // Replace with your EmailJS public API key
-})();
