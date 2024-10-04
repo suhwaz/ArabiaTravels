d0b36c4a46717c562759627a1c2b1984b9cb470d
suhwaz <suhail.wazeer@outlook.com>
2024-10-04 15:50:27 +0100

    Updated promo section and made adjustments for responsive design

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,3 @@
-// This is where you can add any interactive JavaScript you might need.
-document.querySelectorAll('.expand-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        const tile = this.parentElement;
-        tile.classList.toggle('expanded');
-        if (tile.classList.contains('expanded')) {
-            tile.querySelector('p').innerText += ' - Detailed information goes here.';
-        } else {
-            tile.querySelector('p').innerText = tile.querySelector('p').innerText.split(' - ')[0];
-        }
-    });
-});
+function toggleDetails(tile) {
+    tile.classList.toggle('active');
+}
